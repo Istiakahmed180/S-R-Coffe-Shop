5dbf68f258fe9bfe9e04f2ea1beb60704d0ba374
Istiakahmed180 <istiakahmed180@gmail.com>
2023-06-21 10:52:49 +0600

    delete login and signup component extra

--- src/Pages/Login/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <div>This Is Login Page</div>;
-};
-
-export default Login;
--- src/Pages/Singup/Signup.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Signup = () => {
-  return <div>This Is Sign Up Page</div>;
-};
-
-export default Signup;
